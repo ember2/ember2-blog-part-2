7291fe7ec72bd445bf4497434b20e377d4998ca3
James Futhey <futhey@gmail.com>
2015-05-11 01:53:37 -0700

    rename index to model

--- app/routes/new.js
+++ app/routes/new.js
@@ -5,20 +5,20 @@ export default Ember.Route.extend({
         createPost () {
             // Creates a reference to our index, so we can
             // capture the values in the #NewPost form
-            let index = this.controllerFor('new');
+            let model = this.controllerFor('new');
 
             // Creates a new post record in ember-data's data-store
             let post = this.store.createRecord('post', {
-                title: index.get('title'),
-                text: index.get('text'),
-                author: index.get('author'),
+                title: model.get('title'),
+                text: model.get('text'),
+                author: model.get('author'),
                 createdDate: new Date()
             });
 
             // Save our new record
             post.save().then(() => {
                 // Reset the form so that each field is blank
-                index.setProperties({
+                model.setProperties({
                     title: '',
                     author: '',
                     text: ''
