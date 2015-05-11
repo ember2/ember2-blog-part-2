1418e47a86ac17fa5534282cb7425339ebbb17ad
James Futhey <futhey@gmail.com>
2015-05-11 01:30:19 -0700

    Updates

---
+++ tests/unit/helpers/breaklines-test.js
@@ -0,0 +1,12 @@
+import {
+  breaklines
+} from '../../../helpers/breaklines';
+import { module, test } from 'qunit';
+
+module('BreaklinesHelper');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = breaklines(42);
+  assert.ok(result);
+});
